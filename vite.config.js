3c14fd8105ffd7315229220d0243ee9b5324387e
xHexed <58384565+xHexed@users.noreply.github.com>
2024-10-01 16:28:41 +0700

    build: migrate away from asp.net for now

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
+/*
 const baseFolder =
     env.APPDATA !== undefined && env.APPDATA !== ''
         ? `${env.APPDATA}/ASP.NET/https`
@@ -32,6 +33,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7160';
+*/
 
 // https://vitejs.dev/config/
 export default defineConfig({
