d687d1bba3ac934a5abd586b93ee2b7b61b523a1
xHexed <58384565+xHexed@users.noreply.github.com>
2024-10-02 12:35:48 +0700

    minor eslint config update

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ export default [
   { 
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020, 
+      ecmaVersion: 'latest', 
       globals: {...globals.browser, ...globals.node},
       parserOptions: {
         ecmaVersion: 'latest',
@@ -28,7 +28,6 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
